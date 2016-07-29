2fd6ab436427c9c58f33df03462de74981905f1b
dragon <alsodragon@gmail.com>
2016-07-29 20:06:05 +0800

    Revert "Revert "Add_a_layout""

---
+++ app/src/main/java/com/vuforia/samples/VuforiaSamples/app/youxiwang/ActivityBuffer.java
@@ -0,0 +1,35 @@
+package com.vuforia.samples.VuforiaSamples.app.youxiwang;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+import com.vuforia.samples.VuforiaSamples.R;
+import com.vuforia.samples.VuforiaSamples.ui.ActivityList.ActivityLauncher;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class ActivityBuffer extends Activity
+{
+    //    启动页延迟显示时间
+    private static long SPLASH_MILLIS = 1950;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
+        setContentView(R.layout.activity_buffer);
+    }
+
+}
